50bdc9fb401ad5a393634d13ecc2ee017755d3a3
Your Name <git config --global user.email you@example.com>
2022-10-03 09:19:54 +0300

    header layout started

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import s from './Header.module.css'
 
 const Header = () => {
     return (
-        <div className={'header_block'}>
-
+        <div className={s.header_block}>
+            <div className={s.btns}>
+            <ul>
+                <li>Logo</li>
+                <li>Filters</li>
+                <li>Help</li>
+                <li>Contact</li>
+                <li>Info</li>
+            </ul>
+            </div>
         </div>
     );
 }
